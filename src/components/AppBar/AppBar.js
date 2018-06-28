1b4038ae5abdd29598e1d5234999b1df9bf978a2
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-27 20:07:00 -0700

    chore: allow user to return to home page

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -13,27 +13,61 @@ import * as actions from '../../store/actions/actions';
 class appBar extends Component {
 
     state = {
-        loginTime: false
+        goLogin: false,
+        goHome: false,
+        goResume: false,
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({ loginTime: false })
+        this.setState({ 
+            goLogin: false,
+            goHome: false ,
+            goResume: false,
+        })
     }
 
     login = () => {
-        this.setState({ loginTime: true })
+        this.setState({ goLogin: true })
+    }
+
+    home = () => {
+        this.setState({ goHome: true })
+    }
+
+    resume = () => {
+        this.setState({ goResume: true })
     }
 
     render () {
 
         return (
             <div className={classes.root}>
-                {this.state.loginTime ? <Redirect to='/login'/> : null }
+                {this.state.goLogin ? <Redirect to='/login'/> : null }
+                {this.state.goHome ? <Redirect to='/' /> : null }
+                {this.state.goResume ? <Redirect to='/game' /> : null }
                 <AppBar position="static">
                     <Toolbar className={classes.bar}>
                         <Typography variant="title" color="inherit" className={classes.flex}>
                             Boba Shop Games
                         </Typography>
+
+                        { this.props.path === '/game'
+                            ? <Button
+                                variant="contained" 
+                                color="secondary" 
+                                onClick={this.home} >Home</Button>
+                            : null 
+                        }
+
+                        {/* { this.props.path === '/'
+                            ? <Button
+                                variant="contained" 
+                                color="secondary" 
+                                onClick={this.resume} >Resume</Button>
+                            : null 
+                        } */}
+
+
                         { this.props.isAuth || this.props.loggingIn
                             ? null 
                             : <Button
@@ -47,7 +81,8 @@ class appBar extends Component {
                                 variant="contained" 
                                 color="secondary" 
                                 onClick={this.props.logout} >Logout</Button>
-                            : null }
+                            : null 
+                        }
                             
                     </Toolbar>
                 </AppBar>
@@ -60,7 +95,7 @@ const mapStateToProps = state => {
     return {
         isAuth: state.redAuth.token !== null,
         loggingIn: state.redAuth.loggingIn,
-        lastPage: state.redAuth.authRedirectPath,
+        path: state.redAuth.authRedirectPath,
     }
 }
 
