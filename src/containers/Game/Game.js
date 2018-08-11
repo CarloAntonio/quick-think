ac5b1a72bb4fa7bc93c56df92c2d3ea386ca5857
Carlo Bilbao <cbilbao88@gmail.com>
2018-08-10 17:20:17 -0700

    remove unused imports

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import CountdownClock from 'react-countdown-clock';
 
 import LikeIcon from '@material-ui/icons/ThumbUp';
